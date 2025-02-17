8f05c4bdad2a020ea880643290f05028fdf9d2b2
Harendra Kumar Das <63789292+hkdas5464@users.noreply.github.com>
2025-02-18 01:52:40 +0530

    GitUpdate navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -107,7 +107,7 @@ export const Navbar = () => {
       </NavbarContent>
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal aria-label="Github" href={siteConfig.links.github}>
+        <Link aria-label="Github" href={siteConfig.links.github}>
           <GithubIcon className="text-default-500" />
         </Link>
         <ThemeSwitch />
